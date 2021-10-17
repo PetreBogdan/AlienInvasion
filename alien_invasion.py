4e5480014630aa444bee9ac336cdc0d730b0840c
Palabog <petre.bogdan98@gmail.com>
2021-10-17 19:19:09 +0300

    Added speed variable and full screen and exit game using q

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,8 +11,14 @@ class AlienInvasion:
         pygame.init() # Returns True for game is initialized
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((
-            self.settings.screen_width, self.settings.screen_heigth))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_heigth = self.screen.get_rect().height
+
+        # self.screen = pygame.display.set_mode((
+        #     self.settings.screen_width, self.settings.screen_heigth
+        # ))
+
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -35,17 +41,27 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
 
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
 
+
+    def _check_keydown_events(self, event): # Refractoring again
+        '''Keypresses'''
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q: # Exits the game when q was pressed
+            sys.exit()
+
+    def _check_keyup_events(self, event): # Refractoring again
+        '''Keypresses'''
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         # Rewrite the screen for every loop in the run_game()
--- ship.py
+++ ship.py
@@ -8,13 +8,17 @@ class Ship:
         """Initialize the ship and starting position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
-        #Incarca imaginea nava
+        # Loading the ship image
         self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
 
-        #Incepe jocul mereu cu o noua nava in centru jos
+        # Starting game with a ship at the bottom
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store de decimal value
+        self.x = float(self.rect.x)
 
         # Movement flag
         self.moving_right = False
@@ -26,7 +30,11 @@ class Ship:
 
     def update(self):
         """Updates the ship movement based on the movement flag"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # Update the speed
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
